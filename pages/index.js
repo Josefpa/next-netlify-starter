167e7b02eefa22e8d682caf0c02793c0a5bea8dc
Josefpa <spt5523@sptechnology.cc>
2022-08-22 22:22:11 -0400

    Test 2

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Test" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Testing of this <code>pages/index.js</code>
         </p>
       </main>
 
